411d52dc2e3ab192099ac8ae7431c4a9183f6b6a
JonasDevDE <73913242+JonasDevDE@users.noreply.github.com>
2025-07-29 16:03:34 +0200

    fix: update entrypoint.sh
    Signed-off-by: JonasDevDE <73913242+JonasDevDE@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+export LISTEN=${LISTEN:-0.0.0.0}
+export PORT=${PORT:-9876}
+export BASE_URL=${BASE_URL:-/api/v0}
+export MODEL=${MODEL:-small}
+export DEVICE=${DEVICE:-cuda}
+export DEVICE_INDEX=${DEVICE_INDEX:-0}
+export COMPUTE_TYPE=${COMPUTE_TYPE:-int8}
+export BEAM_SIZE=${BEAM_SIZE:-5}
+export TRANSLATE=${TRANSLATE:-yes}
+export LANGUAGE=${LANGUAGE:-}
+
 envsubst < /app/config.yaml.template > /app/config.yaml
 
 exec python /app/remote_faster_whisper.py -c /app/config.yaml
